1d70c3a204c186cc3c6d1987313f0dbce6a98253
reza.gholizadeh <reza.gholizadeh@snapptrip.com>
2025-03-31 03:30:28 +0330

    feat(CORS)

--- routes/routes.go
+++ routes/routes.go
@@ -5,10 +5,19 @@ import (
 	"auth/handlers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // Your frontend URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	// Public routes
 	r.POST("/signup", handlers.CreateProfile)
